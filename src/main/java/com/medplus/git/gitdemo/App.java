d3437b05e38987684152696102d7ec83839dd3ef
TalluruKalyan <tallurukalyan09@gmail.com>
2022-07-13 01:08:27 +0530

    added constant class and changed in App class

--- src/main/java/com/medplus/git/gitdemo/App.java
+++ src/main/java/com/medplus/git/gitdemo/App.java
@@ -1,12 +1,17 @@
 package com.medplus.git.gitdemo;
 
 public class App {
-	public void appData() {
-		System.out.println("this is App Data, just for testing........");
+	public void appData(String data) {
+		
+		if(data.equals(Constant.DATA_TYPE)) {
+			System.out.println("this is get demo");
+		}
+		else {
+			System.out.println("this is get");
+		}
 	}
   public static void main(String[] args) {
-    System.out.println("Hello World!");
     App app = new App();
-    app.appData();
+    app.appData("git");
   }
 }
---
+++ src/main/java/com/medplus/git/gitdemo/Constant.java
@@ -0,0 +1,5 @@
+package com.medplus.git.gitdemo;
+
+public class Constant {
+	public static String DATA_TYPE = "git demo";
+}
