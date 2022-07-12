4c61aa143de57378c00e806a4c299deaca8a4342
Kalyan Kumar <tallurukalyan09.com>
2022-07-13 00:28:01 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/medplus/git/gitdemo/App.java
@@ -0,0 +1,12 @@
+package com.medplus.git.gitdemo;
+
+public class App {
+	public void appData() {
+		System.out.println("this is App Data");
+	}
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    App app = new App();
+    app.appData();
+  }
+}
