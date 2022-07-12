444a0923ab740fe2fadd5acebf6402b69890c412
TalluruKalyan <tallurukalyan09@gmail.com>
2022-07-13 00:34:48 +0530

    update print message in App data method

--- src/main/java/com/medplus/git/gitdemo/App.java
+++ src/main/java/com/medplus/git/gitdemo/App.java
@@ -2,7 +2,7 @@ package com.medplus.git.gitdemo;
 
 public class App {
 	public void appData() {
-		System.out.println("this is App Data");
+		System.out.println("this is App Data, just for testing........");
 	}
   public static void main(String[] args) {
     System.out.println("Hello World!");
